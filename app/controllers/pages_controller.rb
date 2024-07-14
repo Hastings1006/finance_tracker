2589b9baa6721ca942e0da3eebfa866c51dbacc6
your_github_username <your_github_email>
2024-07-14 12:53:14 +0100

    incomes db deleted and date time added to transactions and budget controller update to use transactions and not income+expenses

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,12 +3,9 @@ class PagesController < ApplicationController
     @accounts = Account.where(user_id: current_user.id)
     @total = @accounts.sum(:balance)
 
-    @expenses = Expense.includes(:category).where(user_id: current_user.id)
-    @expenses_by_category = @expenses.group_by { |expense| expense.category_id.to_i }.transform_values do |expenses|
-      expenses.sum { |expense| expense.amount }
 
     @budgets = Budget.where(user: current_user)
-    end
+
   end
 
 
